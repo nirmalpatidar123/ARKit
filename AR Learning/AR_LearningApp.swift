9f81ad75e9e0a4c3bb8e23b349ef923b76e6245b
nirmal-opencrowd <npatidar@opencrowd.com>
2024-02-05 13:21:37 +0530

    Initial Commit

---
+++ AR Learning/AR_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_LearningApp.swift
+//  AR Learning
+//
+//  Created by MacHD on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AR_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
